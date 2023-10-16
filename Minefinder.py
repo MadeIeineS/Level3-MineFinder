cd4c8b575d8a1bfe845683966ae2b200ab83526e
MadeIeineS <78508133+MadeIeineS@users.noreply.github.com>
2023-10-17 11:30:47 +1300

    Update and rename Final.py to Minefinder.py

--- Minefinder.py
+++ Minefinder.py
@@ -2,9 +2,10 @@ import tkinter as tk
 from tkinter import messagebox
 import random
 
-
+# Minesweeper class represents the Minesweeper game logic and interface.
 class Minesweeper:
     def __init__(self, master, rows, columns, num_mines, main_window):
+        # Initialize Minesweeper game settings and create the grid.
         self.master = master
         self.rows = rows
         self.columns = columns
@@ -12,20 +13,17 @@ class Minesweeper:
         self.remaining_mines = num_mines
         self.main_window = main_window
 
-
         self.board = [[0 for _ in range(columns)] for _ in range(rows)]
         self.buttons = [[None for _ in range(columns)] for _ in range(rows)]
 
-
         self.game_over_flag = False
-
 
         self.create_grid()
         self.generate_mines()
         self.calculate_numbers()
 
-
     def generate_mines(self):
+        # Randomly place mines on the grid.
         mines = random.sample(range(self.rows * self.columns), self.num_mines)
         for mine in mines:
             row = mine // self.columns
@@ -33,8 +31,8 @@ class Minesweeper:
             self.board[row][col] = -1
             self.buttons[row][col].config(command=lambda r=row, c=col: self.on_click(r, c))
 
-
     def calculate_numbers(self):
+        # Calculate the number of mines surrounding each cell.
         for row in range(self.rows):
             for col in range(self.columns):
                 if self.board[row][col] != -1:
@@ -44,8 +42,8 @@ class Minesweeper:
                                 if self.board[row + i][col + j] == -1:
                                     self.board[row][col] += 1
 
-
     def create_grid(self):
+        # Create the graphical grid interface.
         for row in range(self.rows):
             for col in range(self.columns):
                 button = tk.Button(
@@ -58,11 +56,10 @@ class Minesweeper:
                 button.grid(row=row, column=col)
                 self.buttons[row][col] = button
 
-
     def on_click(self, row, col):
+        # Handle a cell click event.
         if self.game_over_flag:
             return
-
 
         if self.board[row][col] == -1:
             self.reveal_mines()  # Reveal all mines
@@ -75,8 +72,8 @@ class Minesweeper:
                 text=str(self.board[row][col]), state="disabled"
             )
 
-
     def reveal_empty(self, row, col):
+        # Reveal empty cells and their neighbors.
         if (
             0 <= row < self.rows
             and 0 <= col < self.columns
@@ -92,33 +89,34 @@ class Minesweeper:
                     text=str(self.board[row][col]), state="disabled"
                 )
 
-
     def game_over(self):
+        # Handle the game over condition.
         self.game_over_flag = True
         for row in range(self.rows):
             for col in range(self.columns):
                 if self.board[row][col] == -1:
-                    self.buttons[row][col].config(text="X", state="disabled", bg="#de4e5a")  # Change background color to #de4e5a
-
+                    self.buttons[row][col].config(text="X", state="disabled", bg="#de4e5a")  
+# Change background color to #de4e5a
 
         messagebox.showinfo("Game Over", "You lose!")
         self.main_window.deiconify()  # Show the main window
         self.master.destroy()  # Close the game window
 
-
     def reveal_mines(self):
+        # Reveal all mines when the game ends.
         for row in range(self.rows):
             for col in range(self.columns):
                 if self.board[row][col] == -1:
-                    self.buttons[row][col].config(text="X", state="disabled", bg="#de4e5a")  # Change background color to #de4e5a
+                    self.buttons[row][col].config(text="X", state="disabled", bg="#de4e5a")  
+# Change background color to #de4e5a
 
-
+# square_clicked function handles click events on grid squares.
 def square_clicked(event, buttons, row, col):
     square = event.widget
     square.config(bg="#e2ecf4")
     buttons[row][col].invoke()
 
-
+# grid function creates the grid of squares in the game window.
 def grid(master, buttons):
     squares = []
     for i in range(10):
@@ -138,7 +136,7 @@ def grid(master, buttons):
         squares.append(square_row)
     return squares
 
-
+# create_minesweeper_frame function creates the Minesweeper game frame.
 def create_minesweeper_frame(parent, main_window):
     minesweeper_frame = tk.Frame(parent, bg="#e2ecf4")
     minesweeper_frame.pack(side=tk.TOP, padx=10, pady=10)
@@ -146,26 +144,22 @@ def create_minesweeper_frame(parent, main_window):
     squares = grid(minesweeper_frame, buttons.buttons)
     buttons.squares = squares
 
-
+# game_window function creates the game window and UI elements.
 def game_window():
     master = tk.Toplevel(root)
     master.title("Mine Finder")
     master.geometry("515x595")
 
-
     game_window_base = tk.Canvas(master, width=300, height=300, bg="#fff")
     game_window_base.pack()
-
 
     game_window_base.create_rectangle(
         11, 11, 504, 70, outline="#e2ecf4", fill="#e2ecf4"
     )
 
-
     def back():
         master.withdraw()  # Hide the game window
         root.deiconify()  # Show the main window
-
 
     back_button = tk.Button(
         game_window_base,
@@ -177,39 +171,29 @@ def game_window():
     )
     back_button.pack(side=tk.TOP, padx=10, pady=10)
 
-
     create_minesweeper_frame(master, root)  # Pass the main window reference
 
-
+# how_to_play function displays instructions on how to play the game.
 def how_to_play():
     messagebox.showinfo(
         "How to Play",
-        "1. Click one of the tiles within the grid to start the game\n2. The number in each box represents the number of 'mines' in the surrounding 8 tiles\n3. Right-click to place a flag to signal a mine\n4. Left-click to dig up a tile (only if you believe there is no mine)",
+        "1. Click one of the tiles within the grid to start the game\n2. 10 of the squares in the grid have been assigned the ‘Mine’ value, and once clicked, will end the game\n3. You'll have to try and guess your way to then end of the game, relying on instinct and luck, instead of logical thinking skills",
     )
 
-
+# Main application window.
 root = tk.Tk()
 root.title("Mine Finder")
 
-
+# Main canvas for the game.
 canvas = tk.Canvas(root, width=300, height=300, bg="#2b3f6b")
 canvas.pack()
 
-
-canvas.create_rectangle(10, 10, 293, 293, outline="#fff", fill="#fff")
-canvas.create_rectangle(20, 60, 284, 283, outline="#e2ecf4", fill="#e2ecf4")
-
-
-canvas.create_rectangle(10, 10, 293, 293, outline="#fff", fill="#fff")
-canvas.create_rectangle(55, 30, 250, 130, outline="#e2ecf4", fill="#e2ecf4")
-canvas.create_rectangle(55, 140, 250, 270, outline="#e2ecf4", fill="#e2ecf4")
-
-
+# Title label displayed at the top of the main window.
 title = tk.Label(canvas, text="Mine Finder", font=("@Binate", 24, "bold"), bg="#e2ecf4")
 title.pack()
 canvas.create_window(150, 80, window=title)
 
-
+# Play button to start the game.
 play_button = tk.Button(
     canvas,
     text="Play",
@@ -220,7 +204,7 @@ play_button = tk.Button(
 )
 canvas.create_window(150, 175, window=play_button)
 
-
+# How to play button to display game instructions.
 htp_button = tk.Button(
     canvas,
     text="How to play",
@@ -231,5 +215,5 @@ htp_button = tk.Button(
 )
 canvas.create_window(150, 238, window=htp_button)
 
-
+# Start the main application.
 root.mainloop()
